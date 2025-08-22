c948ffa06b29ca2303aad097ce309afdd64b7662
ricardonerys <150439161+ricardonerys@users.noreply.github.com>
2025-08-22 03:40:21 +0000

    fim

--- primeiro_teste/app/page.js
+++ primeiro_teste/app/page.js
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>Ricardo Nery de Brito Junior <br /> </h1>
+      <h1>Ricardo Nery de Brito Junior</h1>
       <img  src="/nerys.jpg" alt="nerys" width="333" height="380" />
-      <br /> 
       <p>
         Graduando em Ciencia da computação. <br /> 
         Aficionado por robótica <br /> 
